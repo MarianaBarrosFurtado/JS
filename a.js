655fba8486ece98ba906fec6322b25fd8f222856
MarianaBarrosFurtado <74161604+MarianaBarrosFurtado@users.noreply.github.com>
2024-02-05 10:52:20 -0300

    Add files via upload

---
+++ a.js
@@ -0,0 +1,5 @@
+var hotText = "pokedex";
+var URL = "https://pokeapi.co/api/v2/pokemon/pikachu";
+
+console.log("Clique para ver as espeificações do Pikachu" + hotText.link(URL));
+// Clique para retornar à <a href="https://developer.mozilla.org/">MDN</a>
